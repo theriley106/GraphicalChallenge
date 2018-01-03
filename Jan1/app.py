ead1daaffea451ccd13c55ff9393d416a3e05e9e
Theriley106 <christopherlambert106@gmail.com>
2018-01-03 01:40:46 -0500

    Partial map of clemson transit for January 1st

--- Jan1/app.py
+++ Jan1/app.py
@@ -5,25 +5,29 @@ import csv
 app = Flask(__name__)
 
 DATABASE = []
+BUSNUM = raw_input("Bus Number: ")
 
-def returnAllCSV():
-	return glob.glob( 'datapoints/*.csv' )
+def returnAllCSV(busNum):
+	return glob.glob('datapoints/{}.csv'.format(busNum))
 
 def readCSVs():
-	for csvFile in returnAllCSV():
+	for csvFile in returnAllCSV(BUSNUM):
 		with open(csvFile, 'rb') as f:
 			reader = csv.reader(f)
 			your_list = list(reader)
-			for your_listz in your_list:
+			for your_listz in your_list[:100000]:
 				try:
-					DATABASE.append({"Bus": your_listz[0], "Lat": your_listz[1], "Long": your_listz[2]})
-				except:
+					DATABASE.append({"Lat": your_listz[2], "Long": your_listz[1]})
+				except Exception as exp:
+					print exp
 					pass
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template("index.html", DATABASE=DATABASE)
+	return render_template("index.html", DATABASE=DATABASE, BUSNUM=BUSNUM)
 
 if __name__ == "__main__":
 	readCSVs()
+	DATABASE = [dict(t) for t in set([tuple(d.items()) for d in DATABASE])]
+	print(len(DATABASE))
 	app.run(host='0.0.0.0', port=8888)
