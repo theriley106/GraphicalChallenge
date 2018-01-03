4897321ea5ae568398d31ea738444b3985e38557
Theriley106 <christopherlambert106@gmail.com>
2018-01-03 18:16:59 -0500

    gets num of stock

--- Jan2/app.py
+++ Jan2/app.py
@@ -1,6 +1,32 @@
 import csv
+import datetime
+
+DATABASE = []
+
+count = {}
+for num in range(31):
+	count['{0:02d}'.format(num+1)] = 0
+
+#print count
+
+def returnTime(timestamp):
+	return (
+    datetime.datetime.fromtimestamp(
+        int(timestamp)
+    ).strftime('%d')
+	)
 
 def returnAllComments():
 	with open('wsbcomments.csv', 'rb') as f:
 		reader = csv.reader(f)
-		return list(reader)
+		for value in list(reader)[1:]:
+			if 'tesla' in str(value[0]).lower() or 'tsla' in str(value[0]).lower() or '$tsla' in str(value[0]).lower():
+				count[str(returnTime(value[1]))] += str(value[0]).lower().count('tsla')
+				count[str(returnTime(value[1]))] += str(value[0]).lower().count('$tsla')
+				count[str(returnTime(value[1]))] += str(value[0]).lower().count('tesla')
+
+if __name__ == '__main__':
+	returnAllComments()
+	for key, value in count.items():
+		print("{} - {}".format(key, value))
+	
