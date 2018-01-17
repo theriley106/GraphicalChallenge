8150b1e507af7a5b8190f86c1656c9cd512b28f1
Theriley106 <christopherlambert106@gmail.com>
2018-01-17 12:57:04 -0500

    Added GenSQL python script

---
+++ scriptsUsed/GenSQL.py
@@ -0,0 +1,34 @@
+file = raw_input("Filename: ")
+subreddit = raw_input("Subreddit (no /r/) : ")
+saveFileName = raw_input("Save Filename: ")
+listOfWords = open(file).read().split("\n")
+
+
+if raw_input("Select Score (Y/N): ").lower() == 'y':
+	query = '''SELECT
+	  body, created_utc
+
+	FROM [fh-bigquery:reddit_comments.all
+
+	WHERE subreddit = "{}" 
+	AND (LOWER(body) LIKE LOWER("% {} %")'''.format(subreddit, listOfWords[0])
+else:
+	query = '''SELECT
+	  body, created_utc
+
+	FROM [fh-bigquery:reddit_comments.all]
+
+	WHERE subreddit = "{}" 
+	AND (LOWER(body) LIKE LOWER("% {} %")'''.format(subreddit, listOfWords[0])
+
+query = query.replace("\t", "")
+
+for var in listOfWords[1:]:
+	query = query + '\nOR LOWER(body) LIKE LOWER("% {} %")'.format(var)
+
+query = query + ")"
+
+f = open(saveFileName,'w')
+f.write(query)
+f.close()
+
---
+++ scriptsUsed/RedditInteractions.py
@@ -0,0 +1,3 @@
+import
+
+print("This is going to find occurances of a LIST OF WORDS in a NEW-LINE DELIMETED JSON FILE and return NON-NEW LINE Delimited JSON")
