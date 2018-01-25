71dc4a619f2ec0f611d06e4225635d844af0b995
Theriley106 <christopherlambert106@gmail.com>
2018-01-25 00:22:27 -0500

    began adding json interaction to Jan24.py

---
+++ pythonFiles/Jan24.py
@@ -0,0 +1,29 @@
+import glob
+import csv
+import json
+
+primaryDataset = json.load(open(glob.glob('static/Jan24.json')[0], 'rb'))
+
+def returnAllCSV(busNum):
+	return glob.glob('static/Jan1.csv')
+
+def readCSVs():
+	DATABASE = []
+	for csvFile in returnAllCSV(BUSNUM):
+		with open(csvFile, 'rb') as f:
+			reader = csv.reader(f)
+			your_list = list(reader)
+			for your_listz in your_list:
+				try:
+					DATABASE.append({"Lat": your_listz[2], "Long": your_listz[1]})
+				except Exception as exp:
+					print exp
+					pass
+	return DATABASE
+
+def genDatabase():
+	DATABASE = readCSVs()
+	DATABASE = [dict(t) for t in set([tuple(d.items()) for d in DATABASE])]
+	DATABASE = DATABASE[:500]
+	return DATABASE
+
