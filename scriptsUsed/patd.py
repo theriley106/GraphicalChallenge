559125d6f5ab341e823723431f08b90ad6a1d495
Theriley106 <christopherlambert106@gmail.com>
2018-02-09 07:38:20 -0500

    Added PATD Script to Scripts Used

---
+++ scriptsUsed/patd.py
@@ -0,0 +1,19 @@
+import requests
+import bs4
+
+
+def grabSite(url):
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	return requests.get(url, headers=headers)
+
+def grabAllSongs(url):
+	res = grabSite(url)
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+
+
+if __name__ == '__main__':
+	grabAllSongs("https://www.azlyrics.com/p/panicatthedisco.html")
+	res = grabSite()
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+
+
