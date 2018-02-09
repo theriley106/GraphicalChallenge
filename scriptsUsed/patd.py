f6644430187bbe494b83e0a4308ce77fee914c94
Theriley106 <christopherlambert106@gmail.com>
2018-02-09 07:45:23 -0500

    Added the genSongUrl function to patd.py

--- scriptsUsed/patd.py
+++ scriptsUsed/patd.py
@@ -1,21 +1,34 @@
 import requests
 import bs4
+import re
 
 
 def grabSite(url):
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 	return requests.get(url, headers=headers)
 
+def genSongUrl(songTitle, baseUrl='https://www.azlyrics.com/lyrics/panicatthedisco/'):
+	string = ''.join(re.findall('\w+', str(songTitle))).lower()
+	if len(string) > 2:
+		return baseUrl + string
+	else:
+		return None
+
 def grabAllSongs(url):
+	listOfSongs = []
 	res = grabSite(url)
 	page = bs4.BeautifulSoup(res.text, 'lxml')
 	for songTitle in page.select("#listAlbum a"):
-		print songTitle
-
+		title = songTitle.getText()
+		url = genSongUrl(title)
+		if url != None:
+			listOfSongs.append({"Title": title, "URL": url})
+	return listOfSongs
 
 if __name__ == '__main__':
-	grabAllSongs("https://www.azlyrics.com/p/panicatthedisco.html")
+	for var in grabAllSongs("https://www.azlyrics.com/p/panicatthedisco.html"):
+		print var
 	#res = grabSite()
-	page = bs4.BeautifulSoup(res.text, 'lxml')
+	#page = bs4.BeautifulSoup(res.text, 'lxml')
 
 
