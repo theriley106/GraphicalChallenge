39b37094e5fbb46450c29400a267c8ff25ded393
Theriley106 <christopherlambert106@gmail.com>
2018-02-09 09:07:43 -0500

    Added patd lyric scraper

--- scriptsUsed/patd.py
+++ scriptsUsed/patd.py
@@ -1,10 +1,10 @@
 import requests
 import bs4
 import re
-
+import json
 
 def grabSite(url):
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
 	return requests.get(url, headers=headers)
 
 def genSongUrl(songTitle, baseUrl='https://www.azlyrics.com/lyrics/panicatthedisco/{0}.html'):
@@ -17,21 +17,32 @@ def genSongUrl(songTitle, baseUrl='https://www.azlyrics.com/lyrics/panicatthedis
 def grabLyrics(url):
 	res = grabSite(url)
 	page = bs4.BeautifulSoup(res.text, 'lxml')
+	lyrics = "<div>{}</div>".format(str(page).partition("<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->")[2].partition("</div")[0])
+	# I know this is an awful way of doing this ^
+	page = bs4.BeautifulSoup(str(lyrics), 'lxml')
+	return str(page.getText())
+
 
 def grabAllSongs(url):
 	listOfSongs = []
 	res = grabSite(url)
 	page = bs4.BeautifulSoup(res.text, 'lxml')
-	for songTitle in page.select("#listAlbum a"):
-		title = songTitle.getText()
-		url = genSongUrl(title)
-		if url != None:
-			listOfSongs.append({"Title": title, "URL": url})
+	for i, songTitle in enumerate(page.select("#listAlbum a")):
+		if i > 72:
+			try:
+				title = songTitle.getText()
+				url = genSongUrl(title)
+				if url != None:
+					listOfSongs.append({"Title": title, "URL": url, "Lyrics": grabLyrics(url)})
+			except:
+				print("Error on {}".format(i))
+			print("Done with {}".format(i))
 	return listOfSongs
 
 if __name__ == '__main__':
-	for var in grabAllSongs("https://www.azlyrics.com/p/panicatthedisco.html"):
-		print var
+	e = grabAllSongs("https://www.azlyrics.com/p/panicatthedisco.html")
+	with open('data.json', 'w') as outfile:
+		json.dump(e, outfile)
 	#res = grabSite()
 	#page = bs4.BeautifulSoup(res.text, 'lxml')
 
