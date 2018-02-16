dcf1adbf529924a3033da811cf362ea6e1030b7e
Theriley106 <christopherlambert106@gmail.com>
2018-02-15 19:43:23 -0500

    Added the college abbreviations to the program

--- scriptsUsed/countCollege.py
+++ scriptsUsed/countCollege.py
@@ -5,9 +5,10 @@ listOfSchools = open("listOfSchools.txt").read().split("\n")
 for val in listOfSchools:
 	schoolName = val.partition("|")[0].strip()
 	for name in val.split("|"):
-		finalList[schoolName] = name
-
+		finalList[name.lower().strip()] = schoolName.lower().strip()
 print finalList
+while True:
+	print(finalList[raw_input("schoolName: ").lower()])
 '''def getCollege(text):
 	# input string of text and it returns colleges inside
 
