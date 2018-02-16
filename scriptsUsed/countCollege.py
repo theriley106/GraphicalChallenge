7de9c7d68829531545d81b97a8cddf9b630b540b
Theriley106 <christopherlambert106@gmail.com>
2018-02-15 19:41:08 -0500

    Added list of schools

---
+++ scriptsUsed/countCollege.py
@@ -0,0 +1,22 @@
+import json
+DB = {"Total": 0}
+finalList = {}
+listOfSchools = open("listOfSchools.txt").read().split("\n")
+for val in listOfSchools:
+	schoolName = val.partition("|")[0].strip()
+	for name in val.split("|"):
+		finalList[schoolName] = name
+
+print finalList
+'''def getCollege(text):
+	# input string of text and it returns colleges inside
+
+for vals in json.load(open("SATFlair.json")):
+	if vals['author_flair_text'] not in DB:
+		DB[vals['author_flair_text']] = 0
+	DB[vals['author_flair_text']] += 1
+	DB["Total"] += 1
+
+with open('SATData.json', 'w') as fout:
+	json.dump(DB, fout)
+'''
