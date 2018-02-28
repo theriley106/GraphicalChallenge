2a5c181efd130312bc788f99254debe6ca39691a
Theriley106 <christopherlambert106@gmail.com>
2018-02-28 16:20:22 -0500

    Seperated female and male comments

--- pythonFiles/Feb4.py
+++ pythonFiles/Feb4.py
@@ -2,7 +2,16 @@ from textblob import TextBlob
 
 femaleSentiment = []
 maleSentiment = []
-open(glob.glob("MaleTwitterComments.txt")[0], 'rb').read().split('\n')
 
-for open(glob.glob("FemaleTwitterComments.txt")[0], 'rb').read().split('\n')
-TextBlob(var).sentiment.polarity
+for comment in open(glob.glob("FemaleTwitterComments.txt")[0], 'rb').read().split('\n'):
+	femaleSentiment.append(TextBlob(comment).sentiment.polarity)
+
+for comment in open(glob.glob("MaleTwitterComments.txt")[0], 'rb').read().split('\n'):
+	maleSentiment.append(TextBlob(comment).sentiment.polarity)
+
+
+male = float(sum(maleSentiment)) / float(len(maleSentiment))
+female = float(sum(femaleSentiment)) / float(len(femaleSentiment))
+
+
+def
