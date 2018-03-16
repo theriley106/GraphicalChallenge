0dcaa6ecdaa2daab3227d95f2eb913439c5f4449
Theriley106 <christopherlambert106@gmail.com>
2018-03-16 01:21:48 -0400

    Removed print data

--- pythonFiles/Mar18.py
+++ pythonFiles/Mar18.py
@@ -7,6 +7,5 @@ info = json.load(open(glob.glob('static/ratingNum.json')[0], 'rb'))
 for key, value in sorted(info.items(), key=lambda x:x[1], reverse=True)[:10]:
     DATA.append({"Value": float(value["Num"]) / float(value["Total"]), "School": key})
 
-print DATA
 def getDatabase():
     return sorted(DATA, key=lambda k: k['Value'])
