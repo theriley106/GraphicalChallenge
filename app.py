75e0f86b8a5063209fee6563c0f536997fa6d7f9
Theriley106 <christopherlambert106@gmail.com>
2018-04-06 17:09:02 -0400

    Added march 20th

--- app.py
+++ app.py
@@ -389,6 +389,27 @@ def Mar19():
 	distanceFrom = sorted(distanceFrom, key=itemgetter('Distance'), reverse=False)
 	return render_template("zipResponseViz.html", responseTimeData=dataset, MeanIncome=MHH, DistanceFrom=distanceFrom)
 
+@app.route('/Mar20/', methods=['GET'])
+def Mar19():
+	import Mar19
+	# Generates the overall analysis from /all
+	MHH = []
+	# Mean household income
+	dataset = Mar19.ResponseByZipAsLod()
+	# Returns the response time from each zip as a list of dictionaries
+	zipVal = Mar19.returnHousholdIncome()
+	# Returns the household incomes for each zip as a python dict
+	for zipC in Mar19.getZipCodes():
+		# Iterates through all zip codes in the dataset
+		MHH.append({"Zip": zipC, "MHH": int(zipVal[zipC]["Income"].replace(",", ""))})
+		# Appends the values to the mean household income dataset
+	MHH = sorted(MHH, key=itemgetter('MHH'), reverse=False)
+	# Sorts the values
+	distanceFrom = json.load(open("static/distance_from.json"))
+	# Loads the distance dataset
+	distanceFrom = sorted(distanceFrom, key=itemgetter('Distance'), reverse=False)
+	return render_template("distanceFrom.html", responseTimeData=dataset, MeanIncome=MHH, DistanceFrom=distanceFrom)
+
 
 
 
