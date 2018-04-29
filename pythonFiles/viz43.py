e467ec3eefd3825d725e65e3b1f0347bc6678d4c
Theriley106 <christopherlambert106@gmail.com>
2018-04-28 22:11:38 -0400

    Added isadopted to viz43.py

--- pythonFiles/viz43.py
+++ pythonFiles/viz43.py
@@ -1,16 +1,31 @@
 import glob
 import csv
+import re
 
-categories = []
+ageDB = []
 DATABASE = []
 
-with open(glob.glob('static/Jan8.csv')[0], 'rb') as f:
+with open(glob.glob('static/aac_shelter_outcomes.csv')[0], 'rb') as f:
 	reader = csv.reader(f)
 	your_list = list(reader)
 
+def convertToDays(age):
+	try:
+		if 'year' in str(age).lower():
+			return int(re.findall("\d+", str(age))[0]) * 365
+		if 'month' in str(age).lower():
+			return int(re.findall("\d+", str(age))[0]) * 30
+		else:
+			return int(re.findall("\d+", str(age))[0])
+	except:
+		print("Exception detected")
+
+def isAdopted(outcomeString):
+	return ('adopt' in str(outcomeString).lower())
 
 for val in your_list:
-	categories.append(val[2])
+	age = val[0]
+	outcome = val[10]
 
 for category in list(set(categories)):
 	DATABASE.append({"Label": category.replace('&', 'and'), "Value": categories.count(category)})
