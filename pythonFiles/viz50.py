275650c676c24a67d5024ae7785dffc43520b6db
Theriley106 <christopherlambert106@gmail.com>
2018-12-21 15:38:06 -0500

    added interaction with companies file

--- pythonFiles/viz50.py
+++ pythonFiles/viz50.py
@@ -2,8 +2,11 @@ import json
 
 a = []
 
+companies = open("../static/listOfTechCompanies.txt").read().split("\n")
+
 with open("/media/christopher/ssd/cscareerquestionds.json") as f:
     for line in f:
         a.append(json.loads(line))
 
 print len(a)
+print len(companies)
