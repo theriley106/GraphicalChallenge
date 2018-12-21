c93d929b4cc7ed4cf5f253cf6805539fbc7a9796
Theriley106 <christopherlambert106@gmail.com>
2018-12-21 15:44:55 -0500

    Added script to interact

--- pythonFiles/viz50.py
+++ pythonFiles/viz50.py
@@ -1,12 +1,23 @@
 import json
+import re
 
-a = []
+a = {}
 
 companies = open("../static/listOfTechCompanies.txt").read().split("\n")
 
 with open("/media/christopher/ssd/cscareerquestionds.json") as f:
-    for line in f:
-        a.append(json.loads(line))
+	for i, line in enumerate(f):
+		x = re.findall("\w+", json.loads(line)['body'].lower())
+		for val in companies:
+			if val.lower() in x:
+				if val.lower() not in a:
+					a[val.lower()] = 0
+				a[val.lower()] += 1
+		if i % 2000 == 0:
+			print(i)
 
-print len(a)
-print len(companies)
+for key, value in a.iteritems():
+	print("{} - {} Mentions".format(key, value))
+
+with open('cscareerquestions.json', 'w') as fp:
+	json.dump(a, fp)
