8fb29822f8254873cf610443abba21ba67fe7ab4
Christopher Lambert <christopherlambert106@gmail.com>
2023-04-17 22:19:17 -0700

    Updated

--- app.py
+++ app.py
@@ -481,7 +481,11 @@ def llm():
 @app.route('/fetch', methods=["POST"])
 def fetch():
 	info = request.json
-	return jsonify({'output': str(canvas.answer_question(LLM[0], question=info['title'], debug=False))})
+	print(info)
+	title = info['title']
+	if LLM[0] == None:
+		LLM[0] = canvas.fetch_df_for_course("166560")
+	return str(canvas.answer_question(LLM[0], question=title, debug=False))
 
 @app.route('/Feb1/', methods=['GET'])
 def Feb1():
